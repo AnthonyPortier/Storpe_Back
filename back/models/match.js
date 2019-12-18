2c65a9ec4c547e25b566172f2788797e91531a00
Stevie Cutman <steviecut@hotmail.fr>
2019-12-18 19:11:10 +0100

    creation models match + git ignore

---
+++ back/models/match.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+
+const Match = Sequelize.define('match', {
+    equipe_1:{
+        type: DataTypes.STRING
+    },
+    equipe_2:{
+        type: DataTypes.STRING
+    },
+    cote1:{
+        type: DataTypes.INT
+    },
+    cote2:{
+        type: DataTypes.INTs
+    }
+})
