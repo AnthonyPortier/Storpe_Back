dc44effbee8e88f8d7337dbf8b29d51db131e666
Ernibe <ornellazaidi@gmail.com>
2019-12-20 07:25:32 +0100

    set authentif and routes

--- back/index.js
+++ back/index.js
@@ -3,22 +3,21 @@ const models = require('./models')
 const bodyParser= require('body-parser')
 const cors = require('cors')
 const app = express()
-const sequelize = require('sequelize')
-const port = 3000
+const port = 5000
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+const LoginRoute = require('./routes/LoginRoute')
+app.use('/LoginRoute', LoginRoute)
 
     require('./routes/dev')(app)
     require('./routes/user')(app)
     require('./routes/match')(app)
-
-
-
+    require('./routes/pronostic')(app)
 
 models
     .sequelize
-    .sync({force: true})
+    .sync()
     .then(() => app.listen(port, () => console.log(`App listening on port ${port}`)))
